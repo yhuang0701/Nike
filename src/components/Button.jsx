b942ef2e465152c979f261f3997ca512f659a35a
Yichen Huang <yichen41@illinois.edu>
2023-11-22 01:45:09 -0600

    Added Superquality section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,12 +6,12 @@ const Button = ({ label, iconURL }) => {
     rounded-full text-white
     border-coral-red">
       {label}
-      <img
+      {iconURL && <img
         src={iconURL}
         alt="arrow right icon"
         className="ml-2 rounded-full
         w-5 h-5"
-      />
+      />}
     </button>
   )
 }
